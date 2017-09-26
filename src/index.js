285f8ac7e99b9f0077d7ee4140b59b5aca3f4505
G.Ilshat <gaifullin.ilshat@gmail.com>
2017-09-26 12:37:19 +0500

    сборка babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import dateHelper from './helpers/dateHelper'
+import * as numberHelper from './helpers/numberHelper'
+import * as stringHelper from './helpers/stringHelper'
+import * as validateHelper from './helpers/validateHelper'
+
+export {
+    dateHelper, numberHelper, stringHelper, validateHelper
+}
