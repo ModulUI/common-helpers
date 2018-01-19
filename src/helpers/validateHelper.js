b0eade5042d21612edc878c4e1660cd7cfdd7ebb
zakharov-dy <zakharov.dy@mail.ru>
2018-01-19 09:39:11 +0500

    fix isCorrectInn

--- src/helpers/validateHelper.js
+++ src/helpers/validateHelper.js
@@ -26,46 +26,46 @@ export const isEmpty = val => val === '' || val === null || val === undefined;
 
 export const isRequired = text => val => isEmpty(val) ? text : undefined;
 
-export const isCorrectInn = function (INN) {
+export const isCorrectInn = function (inn) {
 	var factor1 = [2, 4, 10, 3, 5, 9, 4, 6, 8];
 	var factor2 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
 	var factor3 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
 	var i = 0;
 	var sum = 0;
 	var sum2 = 0;
-	var Result = false;
+	var result = false;
 	var d;
-	if (INN.length == 0) {
+	if (inn.length == 0) {
 		result = true;
 	}
-	else if (INN.length == 10) { //юр лицо
+	else if (inn.length == 10) { //юр лицо
 		sum = 0;
 		for (i = 0; i <= 8; i++) {
-			d = INN.slice(i, i + 1);
+			d = inn.slice(i, i + 1);
 			sum += d * factor1[i];
 		}
 		sum = sum % 11;
 		sum = sum % 10;
-		result = INN.slice(9, 10) == sum;
+		result = inn.slice(9, 10) == sum;
 
 	}
-	else if (INN.length == 12) {//физ лицо и ИП
+	else if (inn.length == 12) {//физ лицо и ИП
 		sum = 0;
 		for (i = 0; i <= 9; i++) {
-			d = INN.slice(i, i + 1);
+			d = inn.slice(i, i + 1);
 			sum += d * factor2[i];
 		}
 		sum = sum % 11;
 		sum = sum % 10;
 		sum2 = 0;
 		for (i = 0; i <= 10; i++) {
-			d = INN.slice(i, i + 1);
+			d = inn.slice(i, i + 1);
 			sum2 += d * factor3[i];
 		}
 		sum2 = sum2 % 11;
 		sum2 = sum2 % 10;
-		result = INN.slice(10, 11) == sum &&
-			INN.slice(11, 12) == sum2;
+		result = inn.slice(10, 11) == sum &&
+			inn.slice(11, 12) == sum2;
 	}
 	return result;
 };
