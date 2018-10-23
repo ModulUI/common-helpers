74775f5f5021cd162dda5a8d78f2ba53f76ae5a0
Igor Elkanyuk <I.Elkanyuk@modulbank.ru>
2018-10-23 12:14:06 +0500

    Добавлен метод форматирования суммы

--- src/helpers/stringHelper.js
+++ src/helpers/stringHelper.js
@@ -21,3 +21,42 @@ export const pluralize = (number, one, two, five) => {
 	}
 	return five;
 };
+
+const isString = value => typeof value === 'string' || value instanceof String;
+
+/**
+ * Разделение числа на разряды с указанным разделителем
+ */
+const separateDigitsNumber = (number, separator) => {
+    const sep = separator || '\xA0';
+    if (!number && number !== 0) return;
+    return number.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1' + sep);
+};
+
+/**
+ * Замена десятичного разделителя
+ */
+const decimalSeparator = (number, oldSeparator, newSeparator) => {
+    if (!number && number !== 0) return;
+    return number.toString().replace(oldSeparator, newSeparator);
+};
+
+/**
+ * Форматирование суммы
+ * @param {Number,String} value
+ * @param {boolean,string} withoutKopecks
+ * @return {*}
+ */
+export const formatAmount = (value, withoutKopecks) => {
+    if (!value && value !== 0) return;
+
+    if (isString(value))
+        value = parseFloat(value);
+
+    if (withoutKopecks === "auto")
+        withoutKopecks = Math.floor(value) === value;
+
+    return withoutKopecks ?
+        separateDigitsNumber(decimalSeparator(value.toFixed())) :
+        separateDigitsNumber(decimalSeparator(value.toFixed(2), '.', ','));
+};
